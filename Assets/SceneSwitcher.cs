3a8492a4a04b888de84dfb9e57cfd824c6b5c751
Sergiy Klochak <72492804+Sergan4ik@users.noreply.github.com>
2022-10-18 09:45:58 +0300

    Initial commit

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public async void LoadScene(string sceneName)
+    {
+        Debug.Log($"{sceneName} start loading");
+        AsyncOperationHandle<SceneInstance> asyncOperationHandle = Addressables.LoadSceneAsync(sceneName);
+        StartCoroutine(asyncOperationHandle);
+        await asyncOperationHandle.Task;
+        Debug.Log($"{sceneName} loaded");
+    }
+
+    IEnumerator ConsoleLoader(AsyncOperationHandle ao)
+    {
+        while (!ao.IsDone)
+        {
+            Debug.Log($"Loading level... {ao.PercentComplete * 100}%");
+            yield return null;
+        }
+    }
+}
